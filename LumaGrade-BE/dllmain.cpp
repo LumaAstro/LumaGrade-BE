3e7fe736fb1e3c52d27f80d3c59ea78707bb118b
Christian Brendlin <ch@brendlinonline.de>
2025-01-19 16:51:33 +0100

    F

--- LumaGrade-BE/dllmain.cpp
+++ LumaGrade-BE/dllmain.cpp
@@ -1,4 +1,4 @@
-// dllmain.cpp : Defines the entry point for the DLL application.
+// dllmain.cpp : Defines the entry point for the DLL application. Esd
 #include "pch.h"
 
 BOOL APIENTRY DllMain( HMODULE hModule,
